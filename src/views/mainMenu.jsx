c2c513799ecdc3888a15c9a752cbec27153551b7
juanfranfdezGit <jfranromfdez@gmail.com>
2025-03-05 16:11:26 +0100

    opening menu first commit
    commit with first version of opening menu with functional start button

--- src/views/mainMenu.jsx
+++ src/views/mainMenu.jsx
@@ -1,4 +1,3 @@
-import PokemonService from '../services/pokeService'
 import { useEffect, useState } from "react";
 
 const backgrounds = [
@@ -10,19 +9,31 @@ const backgrounds = [
 
 function MainMenu() {
   const [background, setBackground] = useState("");
+  const [goGame, setGoGame] = useState(false);
 
   useEffect(() => {
     const randomBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
     setBackground(randomBg);
   }, []);
 
+  function handleGoGame() {
+    setGoGame(!goGame);
+  }
+
   return (
     <>
-      <div className="mainMenu" style={{ backgroundImage: `url(${background})` }}> 
-        <h1 className="mainMenu__title">Pokémon Duels! Card Game</h1>
-        <button className="mainMenu__start buttonLight">Start!</button>
-        <PokemonService /> {/* Renderiza el servicio */}
-      </div>
+      <section className="mainMenu" style={{ backgroundImage: `url(${background})` }}> 
+        <div className="mainMenu__container">
+          <img className="mainMenu__container-logo" src="/assets/images/pokemonLogo.png" alt="Pokemon logo" />
+          <h1 className="mainMenu__container-title">Duels!</h1>
+          <button onClick={handleGoGame} className="mainMenu__container-button">Start!</button>
+        </div>
+      </section>
+
+      <section className={`gameMenu ${goGame ? "goGame" : ""}`}>
+        <h2>IN GAME</h2>
+        <button onClick={handleGoGame}>OUT</button>
+      </section>
     </>
   );
 }
